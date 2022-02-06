1ea4396da551807e4f5e913f3f9cd0dea6add105
samed232323 <samed.hbo23@gmail.com>
2022-02-06 21:33:25 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Error_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayı;
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz:");
+                sayı = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sayı: " + sayı);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem bitti");
+            }
+        }
+    }
+}
